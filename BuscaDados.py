02b63c881e8e78c3b94d85ca2ee1327496415985
Murillo <6d.rocha@gmail.com>
2020-08-03 01:00:53 -0300

    Primeiros arquivos

---
+++ BuscaDados.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/79.0'}
+proxies = {"http": "127.0.0.1:8080", "https": "127.0.0.1:8080"}
+
+class BuscaDados():
+    @classmethod
+    def jogadores(cls):
+        url = 'https://api.cartolafc.globo.com/atletas/mercado/'
+#       resposta = requests.get(url, proxies=proxies, headers=headers, verify=False)
+        resposta = requests.get(url, headers=headers)
+        #print(resposta.content)
+        r = resposta.content
+        jogado = r.decode('utf-8')
+        #print(jogado)
+        return jogado
+
+    @classmethod
+    def statusmercado(cls):
+        url = 'https://api.cartolafc.globo.com/mercado/status'
+        resposta = requests.get(url, headers=headers)
+        r = resposta.content
+        statusmer = r.decode('utf-8')
+        print(statusmer)
+        return statusmer
+
+    @classmethod
+    def partidas(cls):
+        url = 'https://api.cartolafc.globo.com/partidas'
+        resposta = requests.get(url, headers=headers)
+        r = resposta.content
+        jogos = r.decode('utf-8')
+        print(jogos)
+        return jogos
+
+#BuscaDados.jogadores()
+#BuscaDados.statusmercado()
+#BuscaDados.partidas()
---
+++ Filtragem.py
@@ -0,0 +1,20 @@
+import json
+from BuscaDados import BuscaDados
+
+class Filtragem():
+    @classmethod
+    def Provaveis(cls):
+        lista = BuscaDados.jogadores()
+        lista = json.loads(lista)
+        lista = lista['atletas']
+        print(lista)
+        provaveis = []
+        for i in lista:
+            if i['status_id'] == 7:
+                provaveis.append(i)
+            else:
+                pass
+        print(provaveis)
+        return provaveis
+
+Filtragem.Provaveis()
